7c211809b08c8d98c931e942e8fe6f8dbcedf57f
MOUMID_MOHAMED <164639298+MOHAMED-MOUMID@users.noreply.github.com>
2024-04-27 21:08:35 +0100

    Dentiste V2

--- Dentiste.java
+++ Dentiste.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
 
-public class Dentiste extends Personnel {
+public class Dentiste extends Admin {
 	
 	private static List<Personnel> listePersonnel = new ArrayList<>();
 	
 	private int num_dentiste;
 	
-	public Dentiste(int id_pers, String nom_pers, String prenom_pers, String cIN_pers, Date date_naissance_pers,int tele_pers, String email_pers, String adresse_pers, String position_pers, double salaire,int num_dentiste) {
-		super(id_pers, nom_pers, prenom_pers, cIN_pers, date_naissance_pers, tele_pers, email_pers, adresse_pers,position_pers, salaire);
+	public Dentiste(int id_pers, String nom_pers, String prenom_pers, String cIN_pers, Date date_naissance_pers,int tele_pers, String email_pers, String adresse_pers, String position_pers, double salaire, int id_user,String login, String password, int num_dentiste) {
+		super(id_pers, nom_pers, prenom_pers, cIN_pers, date_naissance_pers, tele_pers, email_pers, adresse_pers,position_pers, salaire, id_user, login, password);
 		this.num_dentiste = num_dentiste;
 	}
 	
